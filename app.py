64a0333b85640ae53ebeb8f467e14185282b97e1
srodolfobarbosa-afk <srodolfobarbosa@gmail.com>
2025-09-04 05:21:38 +0000

    Atualizar o app.py
    correção no arquivo

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from app import create_app, socketio
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__)
 
-# A linha abaixo é para desenvolvimento local e não deve ser usada em produção com Gunicorn.
-# if __name__ == "__main__":
-#    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
-
-# Para deploy com Gunicorn, o Gunicorn irá importar e executar a instância \'app\' diretamente.
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
